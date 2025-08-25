7e0c07de7f9c8b92a6951dbf161214c0e397ff5d
thehalbandge <logzii123@gmail.com>
2025-08-25 10:19:44 +0530

    added more in chapter 3

---
+++ 03_cpp_basics/03_area_of_triangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int base, height;
+    cin >> base >> height;
+
+    float area = 0.5 * (base * height);
+    cout << "Area of the triangle = " << base * height << endl;
+
+    return 0;
+}
---
+++ 03_cpp_basics/05_area_of_circle.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int radius;
+    cout << "Enter the radius:";
+    cin >> radius;
+
+    float area;
+    area = M_PI * radius * radius;
+    cout << "Area of the circle is " << area;
+
+    return 0;
+}
